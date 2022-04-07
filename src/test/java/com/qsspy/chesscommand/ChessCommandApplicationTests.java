3a85f2863b1bcb45db292eda72e729b1176acb7d
Daniel <danel.filipek@gmail.com>
2022-04-07 20:32:58 +0200

    [FEATURE] - Added configuration

--- src/test/java/com/qsspy/chesscommand/ChessCommandApplicationTests.java
+++ src/test/java/com/qsspy/chesscommand/ChessCommandApplicationTests.java
@@ -2,8 +2,10 @@ package com.qsspy.chesscommand;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ChessCommandApplicationTests {
 
 	@Test
