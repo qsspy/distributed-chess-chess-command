4665e129657fdef301398ee33ab0be601ce98fe2
Daniel <danel.filipek@gmail.com>
2022-04-07 20:12:34 +0200

    init commit

---
+++ src/main/java/com/qsspy/chesscommand/ChessCommandApplication.java
@@ -0,0 +1,13 @@
+package com.qsspy.chesscommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessCommandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessCommandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qsspy/chesscommand/ChessCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsspy.chesscommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessCommandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
